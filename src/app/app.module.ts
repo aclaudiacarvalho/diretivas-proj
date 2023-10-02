bd2145cf47c4f0402b0caa1bb3b18e485061ccf3
aclaudiacarvalho <aclaudia_carvalho@outlook.com>
2023-10-02 20:31:29 -0300

    finalização components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {FormsModule} from '@angular/forms'
-
+import{ListaModule}from './lista/lista.module'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -17,7 +17,8 @@ import { CompAtributosComponent } from './comp-atributos/comp-atributos.componen
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    ListaModule
   ],
   providers: [],
   bootstrap: [AppComponent]
